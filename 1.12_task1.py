c9fd0f087b143ad2cb2422277cb39f319b36f62d
Anfuehrungszeichenx2 <Anfuehrungszeichenx2@0x2222.de>
2023-12-01 11:16:55 +0100

    Task1

---
+++ 1.12_task1.py
@@ -0,0 +1,21 @@
+import sys
+
+if __name__ == "__main__":
+    out = 0
+    file = open('task1.input',mode='r')
+    lines = file.read()
+    lines = lines.splitlines()
+    for line in lines:
+
+        tmp = ''
+        for i in line:
+            if i.isnumeric():
+                tmp += i
+        if len(tmp) == 1:
+            tmp += tmp
+        if len(tmp) > 2:
+            out += int(tmp[0] + tmp[-1])
+            continue
+        print(tmp)
+        out += int(tmp)
+    print(out)
