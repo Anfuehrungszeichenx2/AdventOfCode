b0ddbfe0fc0de3a075642ceae8798a2f44d6fb7b
Jakob <vanilla@vanilla-chan.de>
2023-12-13 00:32:52 +0100

    sucks

--- Task20/Task20.py
+++ Task20/Task20.py
@@ -9,31 +9,56 @@ if __name__ == "__main__":
     lns = []
     a=0
     b=0
+    current = ''
+    lok = ''
     for i,line in enumerate(lines):
         tmp = []
         for j,char in enumerate(line):
             tmp.append(char)
+        lns.append(tmp)
+
+    for i,line in enumerate(lines):
+        for j,char in enumerate(line):
+        
             if char == 'S':
                 a = i
                 b = j
+                if lns[a][b+1] == '-' or lns[a][b+1] == 'J' or lns[a][b+1] == '7':
+                    current = lns[a][b+1]
+                    b += 1
+                    lok = 'left'
 
-        lns.append(tmp)
-
+                elif lns[a][b-1] == '-' or lns[a][b-1] == 'L' or lns[a][b-1]== 'F':
+                    current = lns[a][b+1]
+                    b -= 1
+                    lok = 'right'
+                elif lns[a+1][b] == '|' or lns[a+1][b] == 'L' or lns[a+1][b] == 'J':
+                    current = lns[a+1][b]
+                    a += 1
+                    lok = 'top'
+                elif lns[a-1][b] == '|' or lns[a-1][b] == '7' or lns[a-1][b] == 'F':
+                    current = lns[a-1][b]
+                    a -= 1
+                    lok = 'bot'
     loop = False
     graph = []
-    current = '7'
-    lok = ''
     counter = 0
-    a = 25
-    b = 33
-    lok = 'left'
+
     field = []
+
     for i in range(142):
-        field.append([])
+        l = []
+        for i in range(143):
+            l.append(-1)
+        field.append(l)
+    field[a][b] = 9
     while loop == False:
+        field[a][b] =current
+        out = 0
         counter += 1
         if current == 'S':
             loop = True
+            break
 
         elif current == '|':
             if lok == 'top':
@@ -85,16 +110,40 @@ if __name__ == "__main__":
                 lok = 'left'
                 current = lns[a][b+1]
                 b += 1
-        print(a,' ',b)
-        field[a].append(b)
-    out = 0
+
+    counter = 0
+
     for l in field:
         if len(l)>0:
-            for i in range(len(l)-1):
-                if l[i+1] - l[i] > 1:
-                    out += l[i+1] - l[i]
-    print(out)
-    print(int(counter/2))
+            inside = False
+            insidec = 0
+            for i in range(len(l)):
+                cur = l[i]
+                if cur != -1 and cur != '-':
+                    if l[i-1] != -1:
+                        insidec = 2
+
+                    else:
+                        insidec +=1
+
+
+                if not inside:
+
+                    if l[i] != -1:
+                        if insidec % 2 != 0:
+                            inside = True
+                else:
+
+                    if l[i] == -1:
+                        counter +=1
+
+                    elif l[i] != '-':
+                        inside = False
+
+
+
+    print(counter)
+
 
 
 
